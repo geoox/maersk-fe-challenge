3c4ba4352a2212c2e8cd022af8bafa00e761d5e9
geoox <georgedobrin11@gmail.com>
2020-11-12 23:51:07 +0100

    refactoring

--- script.js
+++ script.js
@@ -6,52 +6,53 @@ function assignValues(){
         // mobile
         for(var i=0;i<9;i++){
             document.getElementById('block-'+(i+1)).getElementsByClassName('mob-number')[0].innerHTML = shuffledArr[i];
+            let targetElementColor = document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0];
             if(shuffledArr[i]==1){
-                document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0].style.backgroundColor="#6f98a8";
+                targetElementColor.style.backgroundColor="#6f98a8";
             } else if(shuffledArr[i]==2){
-                document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0].style.backgroundColor="#2b8ead";
+                targetElementColor.style.backgroundColor="#2b8ead";
             } else if(shuffledArr[i]==3){
-                document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0].style.backgroundColor="#2f454e";
+                targetElementColor.style.backgroundColor="#2f454e";
             } else if(shuffledArr[i]==4){
-                document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0].style.backgroundColor="#2a8dac";
+                targetElementColor.style.backgroundColor="#2a8dac";
             } else if(shuffledArr[i]==5){
-                document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0].style.backgroundColor="#2f454e";
+                targetElementColor.style.backgroundColor="#2f454e";
             } else if(shuffledArr[i]==6){
-                document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0].style.backgroundColor="#bfbfbf";
+                targetElementColor.style.backgroundColor="#bfbfbf";
             } else if(shuffledArr[i]==7){
-                document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0].style.backgroundColor="#bfbfbf";
+                targetElementColor.style.backgroundColor="#bfbfbf";
             } else if(shuffledArr[i]==8){
-                document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0].style.backgroundColor="#6f98a8";
+                targetElementColor.style.backgroundColor="#6f98a8";
             } else if(shuffledArr[i]==9){
-                document.getElementById('block-'+(i+1)).getElementsByClassName('mob-color')[0].style.backgroundColor="#2f454e";
+                targetElementColor.style.backgroundColor="#2f454e";
             } 
         }
     } else {
         // web
         for(var i=0;i<9;i++){
-            document.getElementById('block-'+(i+1)).innerHTML = shuffledArr[i];
+            let targetElement =  document.getElementById('block-'+(i+1));
+            targetElement.innerHTML = shuffledArr[i];
             if(shuffledArr[i]==1){
-                document.getElementById('block-'+(i+1)).style.backgroundColor="#6f98a8";
+                targetElement.style.backgroundColor="#6f98a8";
             } else if(shuffledArr[i]==2){
-                document.getElementById('block-'+(i+1)).style.backgroundColor="#2b8ead";
+                targetElement.style.backgroundColor="#2b8ead";
             } else if(shuffledArr[i]==3){
-                document.getElementById('block-'+(i+1)).style.backgroundColor="#2f454e";
+                targetElement.style.backgroundColor="#2f454e";
             } else if(shuffledArr[i]==4){
-                document.getElementById('block-'+(i+1)).style.backgroundColor="#2a8dac";
+                targetElement.style.backgroundColor="#2a8dac";
             } else if(shuffledArr[i]==5){
-                document.getElementById('block-'+(i+1)).style.backgroundColor="#2f454e";
+                targetElement.style.backgroundColor="#2f454e";
             } else if(shuffledArr[i]==6){
-                document.getElementById('block-'+(i+1)).style.backgroundColor="#bfbfbf";
+                targetElement.style.backgroundColor="#bfbfbf";
             } else if(shuffledArr[i]==7){
-                document.getElementById('block-'+(i+1)).style.backgroundColor="#bfbfbf";
+                targetElement.style.backgroundColor="#bfbfbf";
             } else if(shuffledArr[i]==8){
-                document.getElementById('block-'+(i+1)).style.backgroundColor="#6f98a8";
+                targetElement.style.backgroundColor="#6f98a8";
             } else if(shuffledArr[i]==9){
-                document.getElementById('block-'+(i+1)).style.backgroundColor="#2f454e";
+                targetElement.style.backgroundColor="#2f454e";
             } 
         }
     }
-
 }
 
 function shuffle(){
